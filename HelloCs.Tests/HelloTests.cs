67ca33630b070c938c498f12a17fa152aa6d7174
Connor Sullivan <dev.connorjs@gmail.com>
2025-06-09 12:41:21 -0700

    🚧 Hello, C# (#2)
    Adds C# source and test projects with basic Hello, World! utility class.
    Configures build props (based on `connorjs-analyzers`). Includes CSharpier
    for formatting and `SonarAnalyzer.CSharp` for base analyzers.

---
+++ HelloCs.Tests/HelloTests.cs
@@ -0,0 +1,18 @@
+﻿using AwesomeAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Connorjs.Sandbox2025.HelloCs.Tests;
+
+[TestClass]
+public class HelloTests
+{
+	[DataTestMethod]
+	[DataRow(null, "Hello, world!")]
+	[DataRow("", "Hello, world!")]
+	[DataRow(" ", "Hello, world!")]
+	[DataRow("esproj", "Hello, esproj!")]
+	public void ShouldSayHello(string? name, string expected)
+	{
+		Hello.SayHello(name).Should().Be(expected);
+	}
+}
---
+++ HelloCs/Hello.cs
@@ -0,0 +1,18 @@
+﻿namespace Connorjs.Sandbox2025.HelloCs;
+
+/// <summary>
+/// Example “Hello, world!” class.
+/// </summary>
+public static class Hello
+{
+	/// <summary>
+	/// Returns a hello string.
+	/// </summary>
+	/// <param name="name">The name to include. Optional.</param>
+	/// <returns>A hello string.</returns>
+	public static string SayHello(string? name)
+	{
+		var nameToReturn = string.IsNullOrWhiteSpace(name) ? "world" : name;
+		return $"Hello, {nameToReturn}!";
+	}
+}
