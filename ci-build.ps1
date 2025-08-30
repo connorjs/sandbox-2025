544a29b63e90f4aa71906aca222af741f0c4ce8c
Connor Sullivan <dev.connorjs@gmail.com>
2025-08-30 11:45:31 -0400

    🎨 Format code (#5)
    Configures CSharpier formatting and fixes Analyzer violations.
    
    Updates `ci-build.ps1` and pipeline.

--- ci-build.ps1
+++ ci-build.ps1
@@ -1,5 +1,10 @@
-#requires -PSEdition Core
+#!/usr/bin/env pwsh
+
 param(
+	[Alias('c')]
+# The configuration to use. Typically "Debug" or "Release".
+	[string]$configuration = "Release",
+
 	[Alias('v')]
 # Set the MSBuild verbosity level. Allowed values are q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic]
 	[string]$verbosity = "minimal"
@@ -10,22 +15,14 @@ if (-not (Get-Command reportgenerator -ErrorAction SilentlyContinue)) {
 	dotnet tool install --global dotnet-reportgenerator-globaltool
 }
 
-# Helper for printing clickable paths in the terminal
-function Write-ClickablePath {
-	param([Parameter(Mandatory)][string]$Path, [string]$Message = $Path)
-	$abs = (Resolve-Path -LiteralPath $Path).ProviderPath
-	$abs = $abs -replace '\\', '/'
-	$abs = [uri]::EscapeDataString($abs)
-	$uri = [uri]"file:///$abs"
-	Write-Host "`e]8;;$uri`a$Message`e]8;;`a"
-}
-
-# Clean test directory
-Remove-Item -Recurse -Force artifacts/*/test-results -ErrorAction SilentlyContinue
+# Clean artifacts
+Remove-Item -Recurse -Force "artifacts" -ErrorAction SilentlyContinue
 
 # Run the build
-dotnet test --verbosity $verbosity --configuration Release
+dotnet test `
+	--configuration $configuration `
+	--verbosity $verbosity
 
 # Generate coverage report
-reportgenerator -reports:"artifacts/*/test-results/*.cobertura.xml" -targetdir:"artifacts" -reporttypes:"Cobertura;HtmlInline" -verbosity:Warning
-Write-ClickablePath -Path "artifacts/index.html" -Message "📊 Coverage report"
+reportgenerator -reports:"artifacts/*/TestResults/*.cobertura.xml" -targetdir:"artifacts/report" -reporttypes:"Cobertura;HtmlInline" -verbosity:Warning
+Remove-Item "artifacts/report/index.htm"; # Remove `index.htm` in favor of just `index.html` to simplify tab-completion on CLI.
